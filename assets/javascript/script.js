ca5c2558e1088c076ea6666575a088fd3c78ba00
Dr-Wyatt <daniel.ryuichi.wyatt@gmail.com>
2019-04-19 19:17:54 -0500

    Buttons are functional

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,29 @@
 var topic = ["Chelsea", "Liverpool", "FC Barcelona", "Manchester City", "Bayern Munich"];
 
+function displayGifs (){
+    var teamName = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        teamName + "&api_key=M6iVyE1E7bYYuMys8lvE0QI49LoKkuse&limit=10";
+
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response){
+        console.log(response);
+        var results = response.data;
+        for (var j = 0; j < results.length; j++) {
+                var teamDiv = $("<div>");
+                var p = $("<p>");
+                p.text(results[j].rating);
+                var teamImage = $("<img>");
+                teamImage.attr("src", results[j].images.fixed_height.url);
+                teamDiv.append(p);
+                teamDiv.append(teamImage);
+                $("#gifs-view").prepend(teamDiv);
+            }
+      });
+}
+
 function renderButtons() {
     $("#buttons-view").empty();
       for (var i = 0; i < topic.length; i++) {
@@ -17,6 +41,6 @@ $("#add-button").on("click", function(event) {
     renderButtons();
   });
 
-//   $(document).on("click", ".team", displayGifs);
+  $(document).on("click", ".team", displayGifs);
 
 renderButtons();
